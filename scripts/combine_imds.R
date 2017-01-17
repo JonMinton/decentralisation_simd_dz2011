7c1be9dfc6665264c37dfaf9d67adf9b2c935116
JonMinton <nate.minton@gmail.com>
2017-01-17 16:30:22 +0000

    merge some 2015 using Gavin's code; start to run analyses

--- scripts/combine_imds.R
+++ scripts/combine_imds.R
@@ -27,12 +27,20 @@ names(imd_num) <- c("lsoa", "prop_id")
 
 # Now the denominator
 
-imd_denom <- read_excel("data/imd/2015/File_6_ID_2015_Population_denominators.xlsx", sheet = 2)
+imd_denom <- read_excel("data/imd/2015/File_6_ID_2015_Population_denominators.xlsx", 
+  sheet = 2, col_types = rep("text", 9), skip = 1,
+  col_names = c(
+    "lsoa", "lsoa_nm", "lad", "lad_nm", "pop_total", "pop_children", "pop_wage", "pop_older", "pop_wage2"
+  )
+)
 
 imd_denom %>% 
-  .[,c(1, 5)] -> imd_denom
+  select(lsoa, pop_total) -> imd_denom
 
-names(imd_denom) <- c("lsoa", "pop_total") 
+imd_denom %>% 
+  mutate(pop_total = as.numeric(str_replace_all(pop_total, ",", ""))
+         ) -> imd_denom
+
 
 imd_2015 <- imd_num %>% inner_join(imd_denom)
 
@@ -75,13 +83,30 @@ imd_num %>%
 
 # 2007 data 
 
-imd_denom <- read_excel("data/imd/2007/708341 - Population Counts.xls", "Mid-2005 Population at Risk")
+imd_denom <- read_excel(
+  "data/imd/2007/708341 - Population Counts.xls", "Mid-2005 Population at Risk",
+  col_types = rep("text", 12), col_names = T
+  )
 imd_denom %>% .[,c(1, 8)] -> imd_denom
 names(imd_denom) <- c("lsoa", "pop_total")
 
-imd_num <- read_excel("data/imd/2007/All data  ID 2007 29 May 2008 - Income.xls", "Income")
+imd_denom %>% 
+  mutate(pop_total = as.numeric(str_replace_all(pop_total, ",", ""))
+         ) -> imd_denom
+
+
+imd_num <- read_excel(
+  "data/imd/2007/All data  ID 2007 29 May 2008 - Income.xls", "Income",
+  col_types = rep("text", 6), col_names = T                    
+                      )
 imd_num %>% .[,c(1, 6)] -> imd_num
 names(imd_num) <- c("lsoa", "pop_id")
+imd_num %>% 
+  mutate(pop_id2 = str_replace_all(pop_id, ",", ""),
+         pop_id3 = str_replace_all(pop_id, "under 10", "5"),
+         pop_id = as.numeric(pop_id3)
+         ) %>% 
+  select(lsoa, pop_id) -> imd_num
 
 imd_num %>% 
   inner_join(imd_denom) %>% 
@@ -91,7 +116,26 @@ imd_num %>%
 
 # 2004 data 
 
-# No comparable data 
+imd_num <- read_excel(
+  "data/imd/2004/SOA levelid2004 - all imd_scores.xls", "Income"
+)
+imd_num %>% 
+  select(lsoa = SOA, prop_id = `INCOME SCORE`) -> imd_num
+
+imd_denom <- read_excel(
+  "data/imd/2004/soalevel2001 - Population Counts.xls",
+  "SOA estimates - rounded",
+  skip = 3
+)
+imd_denom %>% 
+  select(lsoa = `Lower SOA code`, pop_total = `Total population`) -> imd_denom
+
+
+imd_num %>% 
+  inner_join(imd_denom) %>% 
+  mutate(pop_id = prop_id * pop_total) %>% 
+  mutate(year = 2004) %>% 
+  select(lsoa, year, pop_id, pop_total) -> imd_2004
 
 
 # 2000 data 
@@ -99,12 +143,71 @@ imd_num %>%
 # No comparable data 
 
 
+# Data Stitching code from Gavin ------------------------------------------
+
+
+# changes of LSOAs boundaries
+changes.boundary <- read_csv("data/england_lookup/LSOA01_LSOA11_LAD11_EW_LU.csv")  %>%
+  select(LSOA01CD,LSOA11CD,CHGIND)
+
+
+changes.boundary.1 <- changes.boundary %>% left_join(imd_2015,by = c("LSOA11CD" = "lsoa")) 
+
+
+### we use LSOA boundary in 2001 as baseline geographical units
+# case 1; split--one lsoa was split into two or more lsoa in 2011. in this case, aggregate the COB data in 2011 
+# based on 2001 lsoas
+changes.boundary.1 %>% filter(CHGIND == "S")
+#grepl("*.2011",names(changes.boundary.2))
+
+data.split <- changes.boundary.1 %>% filter(CHGIND == "S") %>%
+  select(LSOA01CD, pop_id, pop_total) %>% 
+  group_by(LSOA01CD) %>%
+  summarise(pop_id = sum(pop_id), pop_total = sum(pop_total))
+
+
+# link back to the
+s.ind <- changes.boundary.1$CHGIND =="S"
+match.ind <- match(changes.boundary.1$LSOA01CD[s.ind],data.split$LSOA01CD)
+changes.boundary.1[s.ind,c("pop_id","pop_total")] <- data.split[match.ind,2:3]
+
+# case 2: two or more LSOAs were merged into a single LSOA in 2011. in this case we need aggregate the 2001 LSOAs accordingly.
+changes.boundary.1 %>% filter(CHGIND == "M")
+
+data.merge <- changes.boundary.1 %>% filter(CHGIND == "M") %>%
+  select(LSOA11CD,pop_id, pop_total) %>%
+  group_by(LSOA11CD) %>%
+  summarise(pop_id = sum(pop_id), pop_total = sum(pop_total))
+
+
+# link back
+m.ind <- changes.boundary.1$CHGIND =="M"
+match.ind <- match(changes.boundary.1$LSOA11CD[m.ind],data.merge$LSOA11CD)
+changes.boundary.1[m.ind,names(data.merge)[-1]] <- data.merge[match.ind,2:3]
+
+
+# select unique ids of LSOAs in 2001
+imd_2015_on2001 <- changes.boundary.1[!duplicated(changes.boundary.1$LSOA01CD),] %>% data.frame
+
+# drop complex boundary changes, they only account for a very small proportion of the data
+#COB.data <- COB.data[!COB.data$CHGIND == "X",]
+COB.data %>% tbl_df() %>% filter(ttwa %in% c("London","Manchester","Sheffield & Rotherham")) %>%
+  filter(CHGIND == "X") %>%
+  group_by(ttwa) %>%
+  summarise(num=length(ttwa))
+
+imd_2015_on2001 <- imd_2015_on2001[!imd_2015_on2001$CHGIND == "X",] %>% tbl_df
+
+imd_2015_on2001 %>% 
+  select(lsoa = LSOA01CD, year, pop_id, pop_total) -> imd_2015_on2001
+
 # Combine found data 
 
 imd_allyears <- bind_rows(
+  imd_2004,
   imd_2007,
   imd_2010,
-  imd_2015
+  imd_2015_on2001
 ) %>% 
   group_by(year, lsoa) %>% 
   summarise(pop_id = sum(pop_id), pop_total = sum(pop_total)) %>% 
@@ -119,51 +222,51 @@ imd_allyears <- bind_rows(
 write_csv(x = imd_allyears, path =  "data/imd/imd_id_tidied.csv")
 
 # test for errors in 2007, 2010, 2015 -------------------------------------
-
-# Any numerators > denoms? 
-
-imd_allyears %>% 
-  mutate(has_error = pop_id > pop_total) %>% 
-  filter(has_error)
-
-# No errors of this sort 
-
-# Now some descriptive stats 
-
-# Can only compare 2007 and 2010
-# proportion in each area income deprived 
-
-imd_allyears %>% 
-  filter(year %in% c(2007, 2010)) %>% 
-  mutate(prop_id = pop_id / pop_total) %>% 
-  group_by(year) %>% 
-  arrange(prop_id)
-
-
-# Variation between two years 
-
-imd_allyears %>% 
-#  filter(year %in% c(2007, 2010)) %>% 
-  mutate(prop_id = pop_id / pop_total) %>% 
-  select(lsoa, year, prop_id) %>% 
-  mutate(year = paste("y", year, sep = "_")) %>%
-  spread(year, prop_id) %>% 
-  ggplot(., aes(x = y_2010, y = y_2015)) +
-  geom_point(shape = ".", alpha = 0.3) + 
-  coord_fixed(xlim = c(0, 1), ylim = c(0, 1), expand = F) + 
-  geom_abline(slope = 1, intercept = 0)
-
-# Some evidence of fall in ID proportions 
-
-
-
-
-
-
-
-imd_allyears %>% 
-  filter(year == 2007) -> tmp
-unique(tmp$lsoa) %>% length()
-
-
-  
+# 
+# # Any numerators > denoms? 
+# 
+# imd_allyears %>% 
+#   mutate(has_error = pop_id > pop_total) %>% 
+#   filter(has_error)
+# 
+# # No errors of this sort 
+# 
+# # Now some descriptive stats 
+# 
+# # Can only compare 2007 and 2010
+# # proportion in each area income deprived 
+# 
+# imd_allyears %>% 
+#   filter(year %in% c(2007, 2010)) %>% 
+#   mutate(prop_id = pop_id / pop_total) %>% 
+#   group_by(year) %>% 
+#   arrange(prop_id)
+# 
+# 
+# # Variation between two years 
+# 
+# imd_allyears %>% 
+# #  filter(year %in% c(2007, 2010)) %>% 
+#   mutate(prop_id = pop_id / pop_total) %>% 
+#   select(lsoa, year, prop_id) %>% 
+#   mutate(year = paste("y", year, sep = "_")) %>%
+#   spread(year, prop_id) %>% 
+#   ggplot(., aes(x = y_2010, y = y_2015)) +
+#   geom_point(shape = ".", alpha = 0.3) + 
+#   coord_fixed(xlim = c(0, 1), ylim = c(0, 1), expand = F) + 
+#   geom_abline(slope = 1, intercept = 0)
+# 
+# # Some evidence of fall in ID proportions 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# imd_allyears %>% 
+#   filter(year == 2007) -> tmp
+# unique(tmp$lsoa) %>% length()
+# 
+# 
+#   
--- scripts/do_lsoa2001_data_management.R
+++ scripts/do_lsoa2001_data_management.R
@@ -1,7 +1,67 @@
 
-# SIMD combined 
 
-imd_combined <- read_csv("data/imd/imd_id_tidied.csv")
+rm(list = ls())
+
+require(pacman)
+
+pacman::p_load(
+  Rcpp,
+  shapefiles,
+  sp,
+  spdep,
+  CARBayes,
+  MCMCpack,
+  truncdist,
+  
+  rgdal,
+  maptools,  
+  
+  readr, readxl,
+  stringr, tidyr, dplyr,
+  purrr,
+  rgeos,
+  ggplot2,
+  RColorBrewer,
+  tmap
+)
+
+# TTWAs of interest 
+
+ttwas_of_interest <- c(
+  "Birmingham", 
+  "Leeds", 
+  "Sheffield and Rotherham", 
+  "Bradford", 
+  "Liverpool",
+  "Manchester",  
+  "Bristol", 
+  "Wakefield and Castleford",
+  "Coventry", 
+  "Leicester", 
+  "Sunderland",     
+  "Nottingham", 
+  "Newcastle and Durham", 
+  "Brighton" 
+)
+
+
+# Load data 
+
+# imd
+dta <- read_csv("data/imd/imd_id_tidied.csv", col_types = "icdd")
+
+# lookup
+lsoa_to_ttwa <- read_csv("data/england_lookup/LSOA01_TTWA01_UK_LU.csv")
+
+# Link LSOAs to TTWAs 
+
+dta %>% 
+  left_join(lsoa_to_ttwa, by = c("lsoa" = "LSOA01CD")) %>% 
+  select(
+    year, lsoa, ttwa = TTWA01CD, ttwa_nm = TTWA01NM, pop_id, pop_total
+  ) -> lsoa_id_by_ttwa
+
+
 
 
 
@@ -15,22 +75,61 @@ imd_combined <- read_csv("data/imd/imd_id_tidied.csv")
 # 6) update 
 
 # Shapefile for lsoa in 2001
-lsoa_2001 <- read_shape(file = "shapefiles/SG_DataZoneBdry_2011/SG_DataZone_Bdry_2011.shp")
+lsoa_2001 <- read_shape(file = "shapefiles/Lower_layer_super_output_areas_(E+W)_2001_Boundaries_(Full_Clipped)_V2/LSOA_2001_EW_BFC_V2.shp")
+names(lsoa_2001)
 
-# Cartogram for 2011 
-dz_2011_cart <- read_shape(file = "shapefiles/dz_2011_cart/dz_2011_cart.shp", current.projection = "longlat")
-#qtm(dz_2011_shp)
 
-# 2011 centroids
+
+churn_chor <- function(TTWA){
+  lsoa_id_by_ttwa %>% 
+    filter(ttwa_nm == TTWA) %>% 
+    select(lsoa, year, pop_id, pop_total) %>%
+    mutate(prop_id = pop_id / pop_total) %>% 
+    mutate(prop_id = ifelse(prop_id > 0.5, 0.5, prop_id)) %>% 
+    select(lsoa, year, prop_id) %>% 
+    spread(year, prop_id) %>% 
+  append_data(
+    shp = lsoa_2001, data = . ,
+    key.shp = "LSOA01CD", key.data = "lsoa", ignore.na = T
+  ) -> shp_jn 
+  
+  shp_jn <- shp_jn[!is.na(shp_jn$`2007`), ]
+  shp_jn %>% 
+    tm_shape(.) + 
+    tm_facets(ncol = 3) + 
+    tm_fill(
+      col = c("2004", "2007", "2010", "2015"), 
+      palette = brewer.pal(10, "Spectral"), 
+      breaks = seq(0, 0.5, by = 0.05), showNA = F
+    ) +
+    tm_legend(legend.show = F) 
+}
+
+churn_chor("Birmingham")
+churn_chor("Leeds")
+
+
+
+
+# 2001 centroids
 ttwa_centroids <- c(
-  Aberdeen = "S01006646",
-  Glasgow = "S01010265",
-  Edinburgh = "S01008677",
-  Dundee = "S01007705",
-  Inverness = "S01010620",
-  Perth = "S01011939",
-  `Falkirk and Stirling` = "S01013067"
+  Birmingham = "E01009150",
+  Leeds = "E01011365",
+  `Sheffield and Rotherham` = "E01008070",
+  Bradford = "E01010844",
+  Liverpool = "E01006650",
+  Manchester = "E01005131",
+  Bristol = "E01014656",
+  `Wakefield and Castleford` = "E01011920",
+  Coventry = "E01009642",
+  Leicester = "E01013644",
+  Nottingham = "E01013869",
+  `Newcastle and Durham` = "E01008397",
+  Kingston = "E01012853",
+  Brighton = "E01016952"
 )
+
+
 
 # using rgeos::gCentroid
 
@@ -38,7 +137,7 @@ calc_distance_to_centres <- function(shp, code_centre){
   gCentroid(shp, byid = T) %>% 
     as(., "data.frame") -> tmp
   
-  data_frame(dz = as.character(shp@data$DataZone), x = tmp$x, y = tmp$y) %>% 
+  data_frame(dz = as.character(shp@data$LSOA01CD), x = tmp$x, y = tmp$y) %>% 
     mutate(centre = dz == code_centre) %>% 
     mutate(distance_to_centre = ((x - x[centre])^2 + (y - y[centre])^2)^0.5) -> output
   output
@@ -46,20 +145,251 @@ calc_distance_to_centres <- function(shp, code_centre){
 
 
 fn <- function(val, nm){
-  dz_2011 %>% 
+  lsoa_2001 %>% 
     calc_distance_to_centres(., val) %>% 
     .[c(1, 5)] -> out 
-  names(out) <- c("datazone", nm)
+  names(out) <- c("lsoa", nm)
   out
 }
 
 # Find nearest centre and distance to nearest centre
 map2(ttwa_centroids, names(ttwa_centroids), fn) %>% 
   reduce(., inner_join) %>% 
-  gather(place, distance, -datazone) %>%
-  arrange(datazone) %>% 
-  group_by(datazone) %>% 
+  gather(place, distance, -lsoa) %>%
+  arrange(lsoa) %>% 
+  group_by(lsoa) %>% 
   mutate(min_distance = min(distance)) %>%
   filter(distance == min_distance) %>% 
   ungroup() %>% 
-  transmute(datazone, nearest_centre = place, distance_to_centre = distance) -> centre_distance
+  transmute(lsoa, nearest_centre = place, distance_to_centre = distance) -> centre_distance
+
+
+################################
+#### Fit the model
+################################
+#### MCMC quantities
+burnin <- 10000
+n.sample <- 20000
+thin <- 10
+n.keep <- (n.sample - burnin)/thin
+
+
+# Now to revise do_model to run all available years 
+
+do_model <- function(place, initial_dist = 18000){
+  
+  this_dist <- initial_dist
+  has_islands <- T
+  
+  while(has_islands){
+    lsoa_2001  %>% 
+      append_data(
+        shp = . , data = centre_distance, 
+        key.shp = "LSOA01CD", key.data = "lsoa"
+      ) %>% 
+      .[.$nearest_centre == place,] %>% 
+      .[.$distance_to_centre <= this_dist,] -> lsoa_city
+    # plot(lsoa_city, main = this_dist)
+    # browser()
+    
+    w <-  poly2nb(lsoa_city)   
+    if (any(card(w) == 0)){
+      this_dist <- this_dist + 1000
+    } else {
+      has_islands <- F
+    }
+  }
+  
+  simd_2011_reweighted %>% 
+    dplyr::select(lsoa, year, pop_total, pop_id) -> tmp
+  
+  tmp %>% 
+    dplyr::select(-pop_id) %>% 
+    mutate(year = paste0("pop_total_", year)) %>% 
+    spread(year, pop_total) -> pops
+  
+  tmp %>% 
+    dplyr::select(-pop_total) %>% 
+    mutate(year = paste0("pop_id_", year)) %>% 
+    spread(year, pop_id) -> incdeps
+  
+  popinc <- pops %>% inner_join(incdeps)
+  rm(tmp, pops, incdeps)
+  
+  
+  lsoa_city %>% 
+    append_data(
+      shp = ., data = popinc,
+      key.shp = "LSOA01CD", key.data = "lsoa",
+      ignore.na = T
+    ) %>% 
+    .[!is.na(.$pop_total_2004),] -> lsoa_city
+  
+  
+  W_nb <- poly2nb(lsoa_city)
+  # distance to allow the 'islands' to be included 
+  W_list <- nb2listw(W_nb, style = "B")
+  W <- nb2mat(W_nb, style = "B")
+  n <- nrow(W)
+  
+  # Need to remove unconnected LSOA01CDs ('islands')
+  
+  
+  
+  #### Format the data
+  Y.mat <- cbind(
+    as.integer(lsoa_city@data$pop_id_2004),
+    as.integer(lsoa_city@data$pop_id_2007), 
+    as.integer(lsoa_city@data$pop_id_2010), 
+    as.integer(lsoa_city@data$pop_id_2015)
+  )
+  
+  N.mat <- cbind(
+    as.integer(lsoa_city@data$pop_total_2004),
+    as.integer(lsoa_city@data$pop_total_2007), 
+    as.integer(lsoa_city@data$pop_total_2010), 
+    as.integer(lsoa_city@data$pop_total_2015)
+  )
+  
+  # denom_too_small <- N.mat[,5] < 1
+  # N.mat[denom_too_small, 5] <- 1
+  # 
+  # Y.mat[denom_too_small, 5] <- 0
+  
+  
+  
+  #### Run the model
+  Y <- Y.mat[,1] ; N <- N.mat[,1]
+  model_01 <- S.CARleroux(Y ~ 1, trials = N, W = W, family = "binomial", burnin = burnin, n.sample = n.sample, thin = thin )  
+  
+  Y <- Y.mat[,2] ; N <- N.mat[,2]
+  model_02 <- S.CARleroux(Y ~ 1, trials = N, W = W, family = "binomial", burnin = burnin, n.sample = n.sample, thin = thin )  
+  
+  Y <- Y.mat[,3] ; N <- N.mat[,3]
+  model_03 <- S.CARleroux(Y ~ 1, trials = N, W = W, family = "binomial", burnin = burnin, n.sample = n.sample, thin = thin )  
+  
+  Y <- Y.mat[,4] ; N <- N.mat[,4]
+  model_04 <- S.CARleroux(Y ~ 1, trials = N, W = W, family = "binomial", burnin = burnin, n.sample = n.sample, thin = thin )  
+  
+
+  #  model$summary.results
+  
+  #### Compute the coordinates and ordering from the city centre
+  dist.order <- order(lsoa_city@data$distance_to_centre)
+  
+  #### Compute the global RCI and D
+  indicators.post <- array(NA, c(n.keep,8))
+  colnames(indicators.post) <- c(
+    "RCI_2004", "RCI_2007","RCI_2010","RCI_2015",
+    "D_2004", "D_2007", "D_2010", "D_2015"
+  )
+  
+  all_2004 <- as.integer(N.mat[,1])
+  all_2007 <- as.integer(N.mat[,2])
+  all_2010 <- as.integer(N.mat[,3])
+  all_2015 <- as.integer(N.mat[,4])
+
+  
+  for(i in 1:n.keep)
+  {
+    ## Compute the probability and fitted values for the ith posterior sample
+    logit_01 <- model_01$samples$beta[i, ] + model_01$samples$phi[i, ]
+    prob_01 <- exp(logit_01) / (1 + exp(logit_01))
+    prob.mat_01 <- matrix(prob_01, nrow=n, byrow=TRUE)
+    fitted.mat_01 <- N.mat[,1] * prob.mat_01
+    # Explore from here 
+    
+    logit_02 <- model_02$samples$beta[i, ] + model_02$samples$phi[i, ]
+    prob_02 <- exp(logit_02) / (1 + exp(logit_02))
+    prob.mat_02 <- matrix(prob_02, nrow=n, byrow=TRUE)
+    fitted.mat_02 <- N.mat[,2] * prob.mat_02
+    
+    
+    logit_03 <- model_03$samples$beta[i, ] + model_03$samples$phi[i, ]
+    prob_03 <- exp(logit_03) / (1 + exp(logit_03))
+    prob.mat_03 <- matrix(prob_03, nrow=n, byrow=TRUE)
+    fitted.mat_03 <- N.mat[,3] * prob.mat_03
+    
+    logit_04 <- model_04$samples$beta[i, ] + model_04$samples$phi[i, ]
+    prob_04 <- exp(logit_04) / (1 + exp(logit_04))
+    prob.mat_04 <- matrix(prob_04, nrow=n, byrow=TRUE)
+    fitted.mat_04 <- N.mat[,4] * prob.mat_04
+    
+
+    ## Compute the RCI for both years
+    indicators.post[i, 1] <- RCI(fitted.mat_01, as.integer(N.mat[,1]), dist.order)
+    indicators.post[i, 2] <- RCI(fitted.mat_02, as.integer(N.mat[,2]), dist.order)
+    indicators.post[i, 3] <- RCI(fitted.mat_03, as.integer(N.mat[,3]), dist.order)
+    indicators.post[i, 4] <- RCI(fitted.mat_04, as.integer(N.mat[,4]), dist.order)
+
+    ## Compute D for both years
+    p_2004 <- prob.mat_01
+    p_2004_av <- sum(p_2004 * all_2004) / sum(all_2004)
+    indicators.post[i, 6] <- sum(all_2004 * abs(p_2004 - p_2004_av)) / (2 * sum(all_2004) * p_2004_av * (1-p_2004_av))   
+    
+    p_2007 <- prob.mat_02
+    p_2007_av <- sum(p_2007 * all_2007) / sum(all_2007)
+    indicators.post[i, 7] <- sum(all_2007 * abs(p_2007 - p_2007_av)) / (2 * sum(all_2007) * p_2007_av * (1-p_2007_av))   
+    
+    p_2010 <- prob.mat_03
+    p_2010_av <- sum(p_2010 * all_2010) / sum(all_2010)
+    indicators.post[i, 8] <- sum(all_2010 * abs(p_2010 - p_2010_av)) / (2 * sum(all_2010) * p_2010_av * (1-p_2010_av))   
+    
+    p_2015 <- prob.mat_04
+    p_2015_av <- sum(p_2015 * all_2015) / sum(all_2015)
+    indicators.post[i, 9] <- sum(all_2015 * abs(p_2015 - p_2015_av)) / (2 * sum(all_2015) * p_2015_av * (1-p_2015_av))   
+    
+
+  }
+  
+  indicators.post  
+}
+
+
+
+
+indicators_birmingham <- do_model("Birmingham")
+
+
+indicators_dundee <- do_model("Dundee")
+indicators_edinburgh <- do_model("Edinburgh")
+indicators_glasgow <- do_model("Glasgow")
+
+# Tidy all indicator draws 
+
+tidy_indicators <- function(mtrx, place){
+  mtrx %>% 
+    data.frame() %>% 
+    tbl_df %>% 
+    mutate(draw = 1:dim(mtrx)[1]) %>% 
+    gather(mp, value, -draw) %>% 
+    mutate(place = place) %>% 
+    separate(mp, into = c("measure", "period")) %>% 
+    dplyr::select(place, measure, period, draw, value)
+}
+
+tind_aberdeen <- tidy_indicators(indicators_aberdeen, "Aberdeen")
+tind_dundee <- tidy_indicators(indicators_dundee, "Dundee")
+tind_edinburgh <- tidy_indicators(indicators_edinburgh, "Edinburgh")
+tind_glasgow <- tidy_indicators(indicators_glasgow, "Glasgow")
+
+tidy_indicators_all <- reduce(
+  list(tind_aberdeen, tind_dundee, tind_edinburgh, tind_glasgow),
+  bind_rows
+)
+
+
+
+
+
+
+
+
+
+# # Cartogram for 2011 
+# lsoa_cart <- read_shape(file = "shapefiles/lsoa_cart/lsoa_cart.shp", current.projection = "longlat")
+# #qtm(lsoa_shp)
+
+
+
+
