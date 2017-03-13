ba2be6f0f7658e766c4ddf48bb7a99e375237418
Jonathan Minton <nate.minton@gmail.com>
2017-03-13 16:52:45 +0000

    do 2011 dzs with new filter condition; do 2001 dzs with new filter conditon

--- scripts/imd_by_ttwa.R
+++ scripts/imd_by_ttwa.R
@@ -363,10 +363,194 @@ dzs_of_interest  <- unique(dz_selected_ttwa$dz)
 # 
 dz_by_dist_to_centres %>%
   filter(dz %in% dzs_of_interest) %>%
-  group_by(dz) %>%
-  filter(distance == min(distance)) %>%
-  ungroup() %>%
+  left_join(lsoa_to_ttwa, by = c("dz" = "LSOA11CD")) %>% 
+  filter(TTWA11NM == place) %>% 
   arrange(dz) %>%
   inner_join(dz_area) -> dz_by_dist_to_centres
 
 write_csv(dz_by_dist_to_centres, "data/dz_2011_by_dist_to_centres.csv")
+
+# filter(lsoa %in% lsoas_of_interest) %>%
+#   left_join(lsoa_to_ttwa, by = c("lsoa" = "LSOA11CD")) %>% 
+#   filter(TTWA11NM == place) %>% 
+
+# scotland on 2001 boundaries  --------------------------------------------
+
+
+
+
+
+# 
+# 
+# 
+# # Shapefile for 2011
+dz_2001 <- read_shape(file = "shapefiles/scotland_2001_datazones/scotland_dz_2001.shp")
+
+
+
+
+# simd
+#dta <- read_csv("data/imd/imd_id_tidied.csv", col_types = "icdd")
+dta <- read_csv("data/simd/simd_combined.csv") 
+
+dta %>% 
+  select(dz_2001 = datazone, year, pop_id = pop_incomedeprived, pop_total = pop_total) ->
+  dta
+
+
+# lookup
+dz_to_ttwa <- read_csv("data/lsoa_2001_ttwa_2001/LSOA01_TTWA01_UK_LU.csv")
+
+
+
+
+# Link LSOAs to TTWAs 
+
+dta %>% 
+  inner_join(dz_to_ttwa, by = c("dz_2001" = "LSOA01CD")) %>% 
+  select(
+    year, dz_2001, ttwa = TTWA01CD, ttwa_nm = TTWA01NM, pop_id, pop_total
+  ) -> dz_id_by_ttwa_2001
+
+
+# Now to reselect the appropriate TTWAs, reaggregating London w/ Heathrow
+
+ttwas_to_keep <- c(
+  # London =     "E30000234", Heathrow = "E30000266", # to then combine again
+  # Manchester =  "E30000239",
+  # Birmingham = "E30000169",
+  # Newcastle =  "E30000245",
+  # Liverpool =  "E30000233",
+  # Leicester =  "E30000230",
+  # Bristol   =  "E30000180",
+  # Sheffield =  "E30000261",
+  # Leeds     =  "E30000229",
+  # Nottingham = "E30000249",
+  # `Warrington and Wigan` = "E30000284",
+  # `Wolverhampton and Walsall` = "E30000288",
+  # Luton = "E30000237",
+  # Cambridge = "E30000186",
+  # Southampton  = "E30000267",
+  # `Guildford and Aldershot`  = "E30000212",
+  # Medway = "E30000242",
+  # Crawley = "E30000196",
+  # Coventry = "E30000195",
+  # Southend = "E30000268",
+  # Oxford = "E30000250",
+  # Portsmouth = "E30000254",
+  # Reading = "E30000256",
+  # # Wales
+  # Cardiff = "W22000024"
+  # Scotland
+  Glasgow = "S22000018", # 2001 code
+  Edinburgh = "S22000012" # 2001 code
+)
+
+dz_id_by_ttwa_2001 %>% 
+  filter(ttwa %in% ttwas_to_keep) %>% 
+  rename(dz = dz_2001) -> 
+  dz_selected_ttwa_2001
+
+# Next tasks is to find distance of each LSOA to the centre 
+
+# Sub-task is then to specify the centres for each ttwa
+
+ttwa_centres <- c(
+  # London      = "E01004736",
+  # Manchester  = "E01033677",
+  # Birmingham  = "E01033620", 
+  # Newcastle   = "E01008397", 
+  # Liverpool   = "E01033760",
+  # Leicester   = "E01032867",
+  # Bristol     = "E01014540", 
+  # Sheffield   = "E01033264",
+  # Leeds       = "E01033010",
+  # Nottingham  = "E01033406",
+  # `Warrington and Wigan` = "E01033300",
+  # `Wolverhampton and Walsall` = "E01010521",
+  # Luton = "E01015794",
+  # Cambridge = "E01032797",
+  # Southampton  = "E01017140",
+  # `Guildford and Aldershot`  = "E01030452",
+  # Medway = "E01016130",
+  # Crawley = "E01031585",
+  # Coventry = "E01009642",
+  # Southend = "E01015852",
+  # Oxford = "E01028549",
+  # Portsmouth = "E01017028",
+  # Reading = "E01016358",
+  # Scottish Cities
+  # Aberdeen = "S01006646",
+  Glasgow = "S01003358", # Closest match to "S01010265",
+  Edinburgh = "S01002131" # Closest match to "S01008677"
+  # Dundee = "S01007705",
+  # Inverness = "S01010620",
+  # Perth = "S01011939",
+  # `Falkirk and Stirling` = "S01013067",
+  # Cardiff
+  # Cardiff = "W0100169" #Cardiff 036B
+)
+
+
+
+# area of each dz 
+
+
+dz_area <- data_frame(
+  dz = as.character(dz_2001@data$zonecode),
+  area = gArea(dz_2001, byid = T)
+)
+
+
+# Create a new function
+# This function should 
+# Inputs: 
+#   - 
+
+# using rgeos::gCentroid
+
+calc_distance_to_centres <- function(shp, code_centre){
+  gCentroid(shp, byid = T) %>% 
+    as(., "data.frame") -> tmp
+  
+  data_frame(dz = as.character(shp@data$zonecode), x = tmp$x, y = tmp$y) %>% 
+    mutate(centre = dz == code_centre) %>% 
+    mutate(distance_to_centre = ((x - x[centre])^2 + (y - y[centre])^2)^0.5) -> output
+  
+  num_centres <- output$centre %>% sum()
+  
+  if(num_centres == 0) {break("No centroid found")} 
+  if(num_centres > 1) {break("Multiple centres found")}
+  output
+}
+
+
+fn <- function(val, nm){
+  dz_2001 %>% 
+    calc_distance_to_centres(., val) %>% 
+    .[c(1, 5)] -> out 
+  names(out) <- c("dz", nm)
+  out
+}
+
+
+# Find nearest centre and distance to nearest centre
+map2(ttwa_centres, names(ttwa_centres), fn) %>% 
+  reduce(., inner_join) %>% 
+  gather(place, distance, -dz) %>%
+  arrange(dz) -> 
+  dz_by_dist_to_centres
+
+dzs_of_interest  <- unique(dz_selected_ttwa_2001$dz)
+# 
+dz_by_dist_to_centres %>%
+  filter(dz %in% dzs_of_interest) %>% 
+  left_join(dz_to_ttwa, by = c("dz" = "LSOA01CD")) %>% 
+  filter(TTWA01NM == place) %>% 
+  ungroup() %>%
+  arrange(dz) %>%
+  inner_join(dz_area) -> dz_by_dist_to_centres
+
+write_csv(dz_by_dist_to_centres, "data/dz_2001_by_dist_to_centres.csv")
+
+
